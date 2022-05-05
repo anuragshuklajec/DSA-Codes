9d6d9bf46fe97f4ad9993f688aec2888733ca33e
Anurag Shukla <76862299+anuragshuklajec@users.noreply.github.com>
2022-05-05 14:51:23 +0530

    Create deleteIthNodeRecursive.py

---
+++ LinkedList/deleteIthNodeRecursive.py
@@ -0,0 +1,43 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+def takeInput():
+    inputList = list(map(int,input().split()))
+    head = None
+    for curr in inputList:
+        if curr == -1:
+            break
+        newNode = Node(curr)
+        if head is None:
+            head = newNode
+            tail = newNode
+        else:
+            tail.next = newNode
+            tail = newNode
+    return head
+
+def printLL(head):
+    while head:
+        print(head.data,end=" -> ")
+        head = head.next
+    print("None")
+
+def insertAtIth(head,i):
+    if i < 0 :
+        return head
+    if head is None:
+        return None
+
+    if i == 0:
+        return head.next
+
+    smallHead = insertAtIth(head.next,i-1)
+    head.next = smallHead
+    return head
+
+head = takeInput()
+printLL(head)
+head = insertAtIth(head,2)
+printLL(head)
