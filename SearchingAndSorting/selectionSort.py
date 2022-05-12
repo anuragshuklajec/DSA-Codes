dfb7d784b7bd8388cdce7f20d66d6c0b3807b9b9
Anurag Shukla <76862299+anuragshuklajec@users.noreply.github.com>
2022-05-12 21:01:09 +0530

    Create selectionSort.py

---
+++ SearchingAndSorting/selectionSort.py
@@ -0,0 +1,11 @@
+def selectionSort(arr):
+    for i in range(len(arr)-1):
+        min_index = i
+        for j in range(i+1,len(arr)):
+            if arr[min_index] > arr[j]:
+                min_index = j
+        arr[min_index],arr[i] = arr[i],arr[min_index]
+arr = list(map(int,input().split()))
+selectionSort(arr)
+print(arr)
+
