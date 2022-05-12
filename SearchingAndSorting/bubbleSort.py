886ce49b4562e4d635378c977c11d5ad12bdc54d
Anurag Shukla <76862299+anuragshuklajec@users.noreply.github.com>
2022-05-12 21:05:38 +0530

    Create bubbleSort.py

---
+++ SearchingAndSorting/bubbleSort.py
@@ -0,0 +1,10 @@
+def bubbleSort(arr):
+    for i in range(len(arr)-1):
+        for j in range(len(arr)-1-i):
+            if arr[j] > arr[j+1]:
+                arr[j],arr[j+1] = arr[j+1],arr[j]
+
+arr = list(map(int,input().split()))
+bubbleSort(arr)
+print(arr)
+
