da55d50d4d39e08904bedc88a084283127fac7cb
Anurag Shukla <76862299+anuragshuklajec@users.noreply.github.com>
2022-05-19 11:00:54 +0530

    Create implementation.py

---
+++ BinaryTree/implementation.py
@@ -0,0 +1,34 @@
+class BinaryTreeNode:
+    def __init__(self,data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+# Line seperated input
+def treeInput():
+    rootData = int(input())
+    if rootData == -1:
+        return None
+    root = BinaryTreeNode(rootData)
+    leftTree = treeInput()
+    rightTree = treeInput()
+    root.left = leftTree
+    root.right = rightTree
+    return root
+
+# Pre order
+def printBinaryTree(root):
+    if root == None:
+        return
+    print(root.data,end=":")
+    if root.left != None:
+        print("L", root.left.data,end=",")
+    if root.right != None:
+        print("R", root.right.data,end="")
+    print()
+    printBinaryTreePreOrder(root.left)
+    printBinaryTreePreOrder(root.right)
+
+
+root = treeInput()
+printBinaryTree(root)
