b6532b58c7c458803afc32f5fd5faf2c4afc2613
Anurag Shukla <76862299+anuragshuklajec@users.noreply.github.com>
2022-05-22 17:58:36 +0530

    Create searchNodeInBST.py

---
+++ BinarySearchTree/searchNodeInBST.py
@@ -0,0 +1,59 @@
+def searchInBST(root,x):
+    if root == None:
+        return False
+    if root.data == x:
+        return True
+    if root.data > x:
+        return searchInBST(root.left,x)
+    else:
+        return searchInBST(root.right,x)
+
+
+import queue
+class BinaryTreeNode:
+    def __init__(self,data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def takeInputLevelWise():
+    q = queue.Queue()
+    print("Enter root")
+    rootData = int(input())
+    if rootData == -1:
+        return None
+    root = BinaryTreeNode(rootData)
+    q.put(root)
+    while not q.empty():
+        current_node = q.get()
+        print("Enter left child of ", current_node.data)
+        leftChildData = int(input())
+        if leftChildData != -1:
+            leftChild = BinaryTreeNode(leftChildData)
+            current_node.left = leftChild
+            q.put(leftChild)
+        print("Enter right child of ", current_node.data)
+        rightChildData = int(input())
+        if rightChildData != -1:
+            rightChild = BinaryTreeNode(rightChildData)
+            current_node.right = rightChild
+            q.put(rightChild)
+    return root
+
+def printTreeLevelWise(root):
+    q = queue.Queue()
+    q.put(root)
+    while not q.empty():
+        node = q.get()
+        print(node.data,end=" ")
+        if node.left is not None:
+            leftChild = node.left
+            q.put(leftChild)
+
+        if node.right is not None:
+            rightChild = node.right
+            q.put(rightChild)
+
+root = takeInputLevelWise()
+x = int(input())
+print(searchInBST(root,x))
