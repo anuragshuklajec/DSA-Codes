f81644464c4eb96c2a3f992a9601dabb3f5e061c
Anurag Shukla <anuragshuklajec@gmail.com>
2024-04-28 18:25:52 +0530

    Time: 53 ms (68.93%), Space: 16.8 MB (9.8%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -8,8 +8,10 @@ class Solution:
             newHead = None
             currHead = None
             rem = 0
-            while l1 and l2:
-                currSum = l1.val + l2.val + rem
+            while l1 or l2:
+                val1 = l1.val if l1 else 0
+                val2 = l2.val if l2 else 0
+                currSum = val1 + val2 + rem
                 rem = currSum // 10
                 to_add = currSum % 10
                 curr_ele = ListNode(to_add)
@@ -19,16 +21,6 @@ class Solution:
                 else:
                     currHead.next = curr_ele
                     currHead = curr_ele
-                l1 = l1.next
-                l2 = l2.next
-
-            while l1 or l2:
-                currSum = l1.val + rem if l1 else l2.val + rem
-                rem = currSum // 10
-                to_add = currSum % 10
-                curr_ele = ListNode(to_add)
-                currHead.next = curr_ele
-                currHead = curr_ele
                 if l1: l1 = l1.next
                 if l2: l2 = l2.next
 
