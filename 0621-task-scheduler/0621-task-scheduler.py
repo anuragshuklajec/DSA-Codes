9606f28b781a070b6b0e544b62d1fa7e1ac4323c
Anurag Shukla <anuragshuklajec@gmail.com>
2024-05-15 23:26:12 +0530

    Time: 411 ms (43.07%), Space: 17.2 MB (10.78%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -9,7 +9,9 @@ class Solution:
 
         while maxHeap or q:
             time+=1
-            if maxHeap:
+            if not maxHeap:
+                time = q[0][1]
+            else:
                 cnt = 1 + heapq.heappop(maxHeap)
                 if cnt:
                     q.append([cnt,time+n])
